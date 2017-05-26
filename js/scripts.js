d14108521663a19dc0c82127f8b6279ec9f77f90
VEDANT KASHYAP <smartvedi@gmail.com>
2017-05-26 12:34:19 +0530

    Added feature to show search results from wikipedia and allowing to browse the wiki pages for those results.

--- js/scripts.js
+++ js/scripts.js
@@ -6,24 +6,33 @@ var query;
 
 function getRandomArticle() {
     query=document.getElementById("searchBar").value;
-    $.getJSON("https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&origin=*&exintro=&explaintext=&titles="+query, function (response) {
-        console.log(JSON.stringify(response));
+    $.getJSON("https://en.wikipedia.org/w/api.php?format=json&action=query&list=search&prop=extracts&origin=*&exintro=&explaintext=&srsearch="+query, function (response) {
+        $("#searchResults").html("");
+        $(".linebreak").css("display","none");
+        var array1=response.query.search;
+        for (var i=0; i<array1.length; i++) {
+            url="http://en.wikipedia.org/wiki/"+array1[i].title.split(" ").join("%20");
+            $("#searchResults").append("<a target='_blank' href="+url+"><li><p id='title'><h2>"+array1[i].title+"</h2></p><p id='summary'>"+array1[i].snippet+"</p></li>");
+        }
+        $("#searchResults").css("display","none");
+        $("#searchResults").slideDown(2000);
+        $(".container").slideDown(2000);
     });
 }
 
 function showSearchBar() {
-    $("#SearchIcon").fadeOut(700);
-    $("#inputGroup").delay(700).fadeIn(700);
-    $("#searchBar").delay(700).fadeIn(700);
-    $(".glyphicon-remove").delay(700).fadeIn(700);
-    $("#submitButton").delay(700).fadeIn(700)
+    $("#SearchIcon").fadeOut(500);
+    $("#inputGroup").delay(500).fadeIn(500);
+    $("#searchBar").delay(500).fadeIn(500);
+    $(".glyphicon-remove").delay(500).fadeIn(500);
+    $("#submitButton").delay(500).fadeIn(500)
     $("#inputGroup").css("display","inline");
 }
 
 function hideSearchBar() {
-    $("#searchBar").fadeOut(700);
-    $(".glyphicon-remove").fadeOut(700);
-    $("#inputGroup").fadeOut(700);
-    $("#submitButton").fadeOut(700);
-    $("#SearchIcon").delay(700).fadeIn(700);
+    $("#searchBar").fadeOut(500);
+    $(".glyphicon-remove").fadeOut(500);
+    $("#inputGroup").fadeOut(500);
+    $("#submitButton").fadeOut(500);
+    $("#SearchIcon").delay(500).fadeIn(500);
 }
